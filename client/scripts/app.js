39e641c511e6f5973a85a1b1cac5e56a998d7cf5
Terry Gunderson <tgunderson25@gmail.com>
2015-05-19 10:17:47 -0500

    adds all posts to the DOM

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,8 +3,10 @@ console.log("Scripts loaded");
 var myApp = angular.module('myApp', []);
 
 myApp.controller('IndexController', ["$scope", function($scope){
+    $scope.posts = [];
     $scope.updatePost = function(){
-        $scope.message = $scope.username + " posted: " + $scope.rant;
+        $scope.rant = $scope.username + " posted: " + $scope.rant;
+        $scope.posts.push($scope.rant);
         $scope.username = null;
         $scope.rant = null;
     }
